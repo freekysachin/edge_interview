3e7c8a8c58a17a1e14d0a8a010d879109972bbfa
Sachin Parmar <freekysachin06@gmail.com>
2025-09-29 12:43:53 +0530

    Create frameProcessor.h

---
+++ partone/frameProcessor.h
@@ -0,0 +1,13 @@
+#pragma once
+
+class ImageFrame;
+
+/*
+  * @class FrameProcessor
+  * @brief Processes ImageFrame object & perform transformation.
+*/
+class FrameProcessor {
+public:
+
+    void process(ImageFrame* frame);
+};
